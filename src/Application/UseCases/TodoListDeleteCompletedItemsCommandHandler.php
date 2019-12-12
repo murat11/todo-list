210d8edf01cbfc1260b68d5102170609261ae625
Murat Erkenov <murat.erkenov@ipresence.ca>
2019-12-12 23:32:55 +0300

    TodoListDeleteCompletedItems use-case handled

---
+++ src/Application/UseCases/TodoListDeleteCompletedItemsCommandHandler.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Application\UseCases;
+
+use App\Application\Repository\TodoListRepositoryAwareInterface;
+use App\Application\Repository\TodoListRepositoryAwareTrait;
+
+class TodoListDeleteCompletedItemsCommandHandler implements TodoListRepositoryAwareInterface
+{
+    use TodoListRepositoryAwareTrait;
+
+    /**
+     * @param TodoListDeleteCompletedItemsCommand $command
+     */
+    public function handle(TodoListDeleteCompletedItemsCommand $command): void
+    {
+        $todoList = $this->todoListRepository->findOneById($command->getListId());
+        $todoList->deleteCompletedItems();
+
+        $this->todoListRepository->save($todoList);
+    }
+}
