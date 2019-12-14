4dca82bc2551a564020ac0616d156e6c05588298
Murat Erkenov <murat@11bits.net>
2019-12-15 02:50:55 +0300

    Serializer added

---
+++ src/Infrastructure/Api/Serializer/TodoListSerializer.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure\Api\Serializer;
+
+use App\Domain\TodoList;
+use App\Infrastructure\Serializer\SerializerInterface;
+
+class TodoListSerializer implements SerializerInterface
+{
+    /**
+     * @var TodoListItemSerializer
+     */
+    private $todoListItemSerializer;
+
+    /**
+     * @param TodoListItemSerializer $todoListItemSerializer
+     */
+    public function __construct(TodoListItemSerializer $todoListItemSerializer)
+    {
+        $this->todoListItemSerializer = $todoListItemSerializer;
+    }
+
+    /**
+     * @param TodoList $data
+     *
+     * @return array
+     */
+    public function serialize($data): array
+    {
+        $output = [
+            'id' => $data->getId(),
+            'name' => $data->getName(),
+            'participants' => $data->getParticipantEmails(),
+            'todos' => array_map([$this->todoListItemSerializer, 'serialize'], $data->getItems()),
+        ];
+
+        return $output;
+    }
+
+    /**
+     * @param $data
+     *
+     * @return bool
+     */
+    public function canSerialize($data): bool
+    {
+        return $data instanceof TodoList;
+    }
+}
---
+++ src/Infrastructure/Serializer/ChainedSerializer.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure\Serializer;
+
+use InvalidArgumentException;
+
+class ChainedSerializer implements SerializerInterface
+{
+    /**
+     * @var SerializerInterface[]|array
+     */
+    private $serializers;
+
+    /**
+     * @param SerializerInterface[] $serializers
+     */
+    public function __construct(array $serializers)
+    {
+
+        $this->serializers = $serializers;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function serialize($data): array
+    {
+        foreach ($this->serializers as $serializer) {
+            if ($serializer->canSerialize($data)) {
+                return $serializer->serialize($data);
+            }
+        }
+
+        throw new InvalidArgumentException(sprintf('Can not find serializer for %s', get_class($data)));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function canSerialize($data): bool
+    {
+        foreach ($this->serializers as $serializer) {
+            if ($serializer->canSerialize($data)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
