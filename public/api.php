f6662675b5c1205d6b77705bd4c5a5e14347a42a
Murat Erkenov <murat@11bits.net>
2019-12-15 17:10:26 +0300

    DeleteTodoListRequestHandler

--- public/api.php
+++ public/api.php
@@ -3,12 +3,14 @@
 use App\Infrastructure\Api\ApiRequest;
 use App\Infrastructure\Api\FrontController;
 use App\Infrastructure\Api\RequestHandlers\CreateTodoListRequestHandler;
+use App\Infrastructure\Api\RequestHandlers\DeleteTodoListRequestHandler;
 
 require '../vendor/autoload.php';
 require '../src/assemble.php';
 
 $routing = [
-    ['POST', 'api.php/lists', CreateTodoListRequestHandler::class]
+    ['POST', 'api.php/lists', CreateTodoListRequestHandler::class],
+    ['DELETE', 'api.php/lists/:list-id', DeleteTodoListRequestHandler::class]
 ];
 
 $apiController = FrontController::createInstance($routing, $requestHandlerFactory);
---
+++ src/Infrastructure/Api/RequestHandlers/DeleteTodoListRequestHandler.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure\Api\RequestHandlers;
+
+use App\Application\UseCases\TodoListDelete\TodoListDeleteCommand;
+use App\Domain\Exception\NotFoundException as DomainNotFoundException;
+use App\Infrastructure\Api\Exceptions\NotFoundException;
+use App\Infrastructure\Repository\Exception\NotFoundException as RepositoryNotFoundException;
+use App\Infrastructure\Api\ApiRequest;
+use App\Infrastructure\Api\ApiRequestHandler;
+use App\Infrastructure\Api\ApiResponse;
+
+class DeleteTodoListRequestHandler extends ApiRequestHandler
+{
+    public function handle(ApiRequest $request): ApiResponse
+    {
+        $arguments = $request->getArguments();
+        $command = new TodoListDeleteCommand($arguments['list-id'] ?? '');
+
+        try {
+            $apiResponse = new ApiResponse(
+                ApiResponse::STATUS_CODE_OK,
+                $this->handleCommand($command)
+            );
+        } catch (DomainNotFoundException|RepositoryNotFoundException $x) {
+            throw new NotFoundException(sprintf('Todo List with ID %s not found', $command->getListId()));
+        }
+
+        return $apiResponse;
+    }
+}
