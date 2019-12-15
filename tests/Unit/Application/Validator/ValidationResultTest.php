4d61c7323364c9a1865fed21968654d7952b38b9
Murat Erkenov <murat@11bits.net>
2019-12-15 14:01:47 +0300

    Use-case validator added

---
+++ tests/Unit/Application/Validator/ValidationResultTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Test\Unit\Application\Validator;
+
+use App\Application\Validator\ValidationResult;
+use PHPUnit\Framework\TestCase;
+
+class ValidationResultTest extends TestCase
+{
+    public function testAdoptOk()
+    {
+        $validationResult = new ValidationResult();
+        $validationResult->addError('Error01');
+        $validationResult->addError('Error11', 'field1');
+        $validationResult->addError('Error12', 'field1');
+        $validationResult->addError('Error21', 'field2');
+
+        $otherResult = new ValidationResult();
+        $otherResult->addError('Error02');
+        $otherResult->addError('Error31', 'field3');
+        $otherResult->addError('Error13', 'field1');
+        $otherResult->addError('Error22', 'field2');
+
+        $validationResult->adoptErrorsFromOtherResult($otherResult);
+        $this->assertEquals(
+            [
+                '_' => ['Error01', 'Error02'],
+                'field1' => ['Error11', 'Error12', 'Error13'],
+                'field2' => ['Error21', 'Error22'],
+                'field3' => ['Error31'],
+            ],
+            $validationResult->getErrors()
+        );
+    }
+}
