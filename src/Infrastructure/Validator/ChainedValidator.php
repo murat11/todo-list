1471ed2405d2fb4e731ca78e55f91fd60e075c0e
Murat Erkenov <murat@11bits.net>
2019-12-15 14:24:16 +0300

    ChainedValidator

---
+++ src/Infrastructure/Validator/ChainedValidator.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure\Validator;
+
+use App\Application\Validator\ValidationResult;
+use App\Application\Validator\ValidatorInterface;
+
+class ChainedValidator implements ValidatorInterface
+{
+    /**
+     * @var ValidatorInterface[]
+     */
+    private $validators;
+
+    /**
+     * @param array $validators
+     */
+    public function __construct(array $validators)
+    {
+        $this->validators = $validators;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validate($data): ValidationResult
+    {
+        /** @var ValidatorInterface[] $applicableValidators */
+        $applicableValidators = array_filter(
+            $this->validators,
+            function (ValidatorInterface $validator) use ($data) {
+                return $validator->canValidate($data);
+            }
+        );
+
+        $jointResult = new ValidationResult();
+        $jointResult->setValid(true);
+
+        foreach ($applicableValidators as $validator) {
+            $result = $validator->validate($data);
+            if ($result->isValid()) {
+                continue;
+            }
+            $jointResult->setValid(false);
+            $jointResult->adoptErrorsFromOtherResult($result);
+        }
+
+        return $jointResult;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function canValidate($data): bool
+    {
+        foreach ($this->validators as $validator) {
+            if ($validator->canValidate($data)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/Unit/Infrastructure/Validator/ChainedValidatorTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Test\Unit\Infrastructure\Validator;
+
+use App\Application\Validator\ValidationResult;
+use App\Application\Validator\ValidatorInterface;
+use App\Infrastructure\Validator\ChainedValidator;
+use PHPUnit\Framework\TestCase;
+
+class ChainedValidatorTest extends TestCase
+{
+    public function testItCanValidateIfOneCan()
+    {
+        $validator1 = $this->createMock(ValidatorInterface::class);
+        $validator1->expects($this->once())->method('canValidate')->with('some-data')->willReturn(false);
+
+        $validator2 = $this->createMock(ValidatorInterface::class);
+        $validator2->expects($this->once())->method('canValidate')->with('some-data')->willReturn(true);
+
+        $this->assertTrue((new ChainedValidator([$validator1, $validator2]))->canValidate('some-data'));
+    }
+
+    public function testItCanNotValidateIfNoneCan()
+    {
+        $validator1 = $this->createMock(ValidatorInterface::class);
+        $validator1->expects($this->once())->method('canValidate')->with('some-data')->willReturn(false);
+
+        $validator2 = $this->createMock(ValidatorInterface::class);
+        $validator2->expects($this->once())->method('canValidate')->with('some-data')->willReturn(false);
+
+        $this->assertFalse((new ChainedValidator([$validator1, $validator2]))->canValidate('some-data'));
+    }
+
+    public function testItValidatedIfAllDid()
+    {
+        $validationResult = $this->createMock(ValidationResult::class);
+        $validationResult->method('isValid')->willReturn(true);
+
+        $validator1 = $this->createMock(ValidatorInterface::class);
+        $validator1->method('canValidate')->with('some-data')->willReturn(true);
+        $validator1->expects($this->once())->method('validate')
+            ->with('some-data')
+            ->willReturn($validationResult);
+
+        $validator2 = $this->createMock(ValidatorInterface::class);
+        $validator2->method('canValidate')->with('some-data')->willReturn(true);
+        $validator2->expects($this->once())->method('validate')
+            ->with('some-data')
+            ->willReturn($validationResult);
+
+        $this->assertTrue((new ChainedValidator([$validator1, $validator2]))->validate('some-data')->isValid());
+    }
+
+    public function testItFailedValidationIfOneFailed()
+    {
+        $validationResult1 = $this->createMock(ValidationResult::class);
+        $validationResult1->method('isValid')->willReturn(true);
+
+        $validationResult2 = $this->createMock(ValidationResult::class);
+        $validationResult2->method('isValid')->willReturn(false);
+        $validationResult2->method('getErrors')->willReturn([]);
+
+        $validator1 = $this->createMock(ValidatorInterface::class);
+        $validator1->method('canValidate')->with('some-data')->willReturn(true);
+        $validator1->expects($this->once())->method('validate')
+            ->with('some-data')
+            ->willReturn($validationResult1);
+
+        $validator2 = $this->createMock(ValidatorInterface::class);
+        $validator2->method('canValidate')->with('some-data')->willReturn(true);
+        $validator2->expects($this->once())->method('validate')
+            ->with('some-data')
+            ->willReturn($validationResult2);
+
+        $this->assertFalse((new ChainedValidator([$validator1, $validator2]))->validate('some-data')->isValid());
+    }
+
+
+    public function testItFailedValidationAndErrorsMerged()
+    {
+        $validationResult1 = $this->createMock(ValidationResult::class);
+        $validationResult1->method('isValid')->willReturn(false);
+        $validationResult1->method('getErrors')->willReturn(['aaa' => ['ccc'], 'ddd' => ['eee']]);
+
+        $validationResult2 = $this->createMock(ValidationResult::class);
+        $validationResult2->method('isValid')->willReturn(false);
+        $validationResult2->method('getErrors')->willReturn(['aaa' => ['bbb']]);
+
+        $validator1 = $this->createMock(ValidatorInterface::class);
+        $validator1->method('canValidate')->with('some-data')->willReturn(true);
+        $validator1->expects($this->once())->method('validate')
+            ->with('some-data')
+            ->willReturn($validationResult1);
+
+        $validator2 = $this->createMock(ValidatorInterface::class);
+        $validator2->method('canValidate')->with('some-data')->willReturn(true);
+        $validator2->expects($this->once())->method('validate')
+            ->with('some-data')
+            ->willReturn($validationResult2);
+
+        $validationResult = (new ChainedValidator([$validator1, $validator2]))->validate('some-data');
+        $this->assertFalse($validationResult->isValid());
+        $this->assertEquals(
+            ['aaa' => ['ccc', 'bbb'], 'ddd' => ['eee']],
+            $validationResult->getErrors()
+        );
+    }
+}
+
