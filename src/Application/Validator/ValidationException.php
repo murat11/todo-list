9b5241345517e2b9d0cdbdca5d9af9f18341a513
Murat Erkenov <murat@11bits.net>
2019-12-15 15:38:40 +0300

    CommandValidator added to CommandBus

---
+++ src/Application/Validator/ValidationException.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace App\Application\Validator;
+
+
+use InvalidArgumentException;
+
+class ValidationException extends InvalidArgumentException
+{
+    /**
+     * @var array
+     */
+    private $validationErrors;
+
+    /**
+     * @param ValidationResult $result
+     *
+     * @return ValidationException
+     */
+    public static function fromValidatorResult(ValidationResult $result): self
+    {
+        return new self($result->getErrors());
+    }
+
+    /**
+     * @param array $validationErrors
+     */
+    public function __construct(array $validationErrors)
+    {
+        parent::__construct("Command validation failed", 400);
+        $this->validationErrors = $validationErrors;
+    }
+
+    /**
+     * @return array
+     */
+    public function getValidationErrors(): array
+    {
+        return $this->validationErrors;
+    }
+}
