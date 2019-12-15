f31dc0cae4e7331b853437d9398acdcadd71ff2f
Murat Erkenov <murat@11bits.net>
2019-12-15 16:32:40 +0300

    TodoListItemCreate and Update command validators

---
+++ src/Application/UseCases/TodoListCreateItem/TodoListCreateItemCommandValidator.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace App\Application\UseCases\TodoListCreateItem;
+
+use App\Application\UseCases\TodoListItemUpdate\TodoListItemUpdateCommand;
+use App\Application\Validator\ValidationResult;
+use App\Application\Validator\ValidatorInterface;
+
+class TodoListCreateItemCommandValidator implements ValidatorInterface
+{
+    /**
+     * @param TodoListCreateItemCommand $data
+     *
+     * @return ValidationResult
+     */
+    public function validate($data): ValidationResult
+    {
+        $validationResult = new ValidationResult();
+        $validationResult->setValid(true);
+        $this->validateTitle($data, $validationResult);
+
+        return $validationResult;
+    }
+
+    /**
+     * @param $data
+     *
+     * @return bool
+     */
+    public function canValidate($data): bool
+    {
+        return $data instanceof TodoListCreateItemCommand || $data instanceof TodoListItemUpdateCommand;
+    }
+
+
+    /**
+     * @param TodoListCreateItemCommand $data
+     * @param ValidationResult $validationResult
+     */
+    private function validateTitle(TodoListCreateItemCommand $data, ValidationResult $validationResult): void
+    {
+        $name = $data->getTitle();
+        if (empty($name)) {
+            $validationResult->setValid(false);
+            $validationResult->addError('Empty title', 'title');
+        }
+    }
+}
