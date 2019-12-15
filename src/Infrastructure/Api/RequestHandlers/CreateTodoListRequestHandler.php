08917661284f7616a489e1f5a64516e214ee91a5
Murat Erkenov <murat@11bits.net>
2019-12-15 16:52:45 +0300

    Most of ApiRequestHandler work moved to base request handler

--- src/Infrastructure/Api/RequestHandlers/CreateTodoListRequestHandler.php
+++ src/Infrastructure/Api/RequestHandlers/CreateTodoListRequestHandler.php
@@ -3,29 +3,20 @@
 namespace App\Infrastructure\Api\RequestHandlers;
 
 use App\Application\UseCases\TodoListCreate\TodoListCreateCommand;
-use App\Application\Validator\ValidationException;
 use App\Infrastructure\Api\ApiRequest;
 use App\Infrastructure\Api\ApiRequestHandler;
 use App\Infrastructure\Api\ApiResponse;
-use App\Infrastructure\Api\Exceptions\BadRequestException;
 
 class CreateTodoListRequestHandler extends ApiRequestHandler
 {
     public function handle(ApiRequest $request): ApiResponse
     {
         $arguments = $request->getArguments();
-
         $command = new TodoListCreateCommand($arguments['name'] ?? '', $arguments['participants'] ?? []);
-
-        try {
-            $todoList = $this->commandBus->handle($command);
-        } catch (ValidationException $x) {
-            throw new BadRequestException($x->getValidationErrors(), $x);
-        }
 
         return new ApiResponse(
             ApiResponse::STATUS_CODE_CREATED,
-            $this->serializer->serialize($todoList)
+            $this->handleCommand($command)
         );
     }
 }
