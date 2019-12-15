b0d32aee9677903ba7fff48e7493da75552f20da
Murat Erkenov <murat@11bits.net>
2019-12-15 19:15:39 +0300

    Mark Multiple Todo Items As Completed/Pending

---
+++ src/Infrastructure/Api/RequestHandlers/ChangeTodoListItemsStatusRequestHandler.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure\Api\RequestHandlers;
+
+use App\Application\UseCases\TodoListChangeItemsStatus\TodoListChangeItemsStatusCommand;
+use App\Infrastructure\Api\Exceptions\NotFoundException;
+use App\Infrastructure\Repository\Exception\NotFoundException as NotFoundRepositoryException;
+use App\Infrastructure\Api\ApiRequest;
+use App\Infrastructure\Api\ApiRequestHandler;
+use App\Infrastructure\Api\ApiResponse;
+
+class ChangeTodoListItemsStatusRequestHandler extends ApiRequestHandler
+{
+    public function handle(ApiRequest $request): ApiResponse
+    {
+        $arguments = $request->getArguments();
+        $completed = $arguments['completed'] ?? null;
+        if (!is_null($completed)) {
+            $completed = (bool) $completed;
+        }
+        $command = new TodoListChangeItemsStatusCommand($arguments['list-id'] ?? '', $completed);
+
+        try {
+            $apiResponse = new ApiResponse(
+                ApiResponse::STATUS_CODE_OK,
+                $this->handleCommand($command)
+            );
+        } catch (NotFoundRepositoryException $x) {
+            throw new NotFoundException(sprintf('Todo List with ID %s not found', $command->getListId()));
+        }
+
+        return $apiResponse;
+    }
+}
