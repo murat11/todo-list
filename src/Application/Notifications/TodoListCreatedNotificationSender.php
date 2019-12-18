8b9235b01cf2b28936b343691177e60c28096bbf
Murat Erkenov <murat@11bits.net>
2019-12-18 12:15:20 +0300

    New Notification sender which uses real smtp and template engine to compose send emails

--- src/Application/Notifications/TodoListCreatedNotificationSender.php
+++ src/Application/Notifications/TodoListCreatedNotificationSender.php
@@ -5,10 +5,10 @@ namespace App\Application\Notifications;
 use App\Domain\EventManager\DomainEventInterface;
 use App\Domain\EventManager\EventHandlerInterface;
 use App\Domain\Events\TodoListCreatedEvent;
-use App\Domain\TodoList;
 
 class TodoListCreatedNotificationSender implements EventHandlerInterface
 {
+
     /**
      * @var NotificationSenderInterface
      */
@@ -31,27 +31,14 @@ class TodoListCreatedNotificationSender implements EventHandlerInterface
             return;
         }
 
-        $notification = $this->createNotificationFromEvent($domainEvent->getTodoList());
-        if (!empty($notification)) {
-            $this->notifier->send($notification);
-        }
-    }
-
-    /**
-     * @param TodoList $todoList
-     *
-     * @return Notification
-     */
-    private function createNotificationFromEvent(TodoList $todoList): ?Notification
-    {
-        $participantEmails = $todoList->getParticipantEmails();
-        if (empty($participantEmails)) {
-            return null;
-        }
-
-        $body = $todoList->getName() . ' ' . $todoList->getId() . PHP_EOL;
-        $body .= json_encode($todoList->getItems());
-
-        return new Notification($participantEmails, 'New Todo List created', $body);
+        $todoList = $domainEvent->getTodoList();
+        $notification = new Notification(
+            $todoList->getParticipantEmails(),
+            TodoListCreatedEvent::NAME,
+            [
+                'todo_list' => $todoList,
+            ]
+        );
+        $this->notifier->send($notification);
     }
 }
---
+++ src/Infrastructure/Notifications/NotificationSender.php
@@ -0,0 +1,82 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure\Notifications;
+
+use App\Application\Notifications\Notification;
+use App\Application\Notifications\NotificationSenderInterface;
+use PHPMailer\PHPMailer\PHPMailer;
+use Twig\Environment as Twig;
+
+class NotificationSender implements NotificationSenderInterface
+{
+    /**
+     * @var Twig
+     */
+    private $twig;
+
+    /**
+     * @var array
+     */
+    private $mailerParameters;
+
+
+    /**
+     * @param array $mailerParameters
+     * @param Twig $twig
+     */
+    public function __construct(array $mailerParameters, Twig $twig)
+    {
+        $this->twig = $twig;
+        $this->mailerParameters = $mailerParameters;
+    }
+
+    /**
+     * @param Notification $notification
+     */
+    public function send(Notification $notification)
+    {
+        foreach ($notification->getRecipients() as $email) {
+            $mailerMessage = $this->prepareMailMessage($email);
+
+            $templatePath = $notification->getType() . '.html.twig';
+            $templateWrapper = $this->twig->load($templatePath);
+            if ($templateWrapper->hasBlock('subject')) {
+                $mailerMessage->Subject = $templateWrapper->renderBlock('subject', $notification->getProperties());
+            }
+
+            if ($templateWrapper->hasBlock('body_html')) {
+                $mailerMessage->Body = $templateWrapper->renderBlock('body_html', $notification->getProperties());
+            }
+
+            if ($templateWrapper->hasBlock('body_text')) {
+                $mailerMessage->AltBody = $templateWrapper->renderBlock('body_text', $notification->getProperties());
+            }
+
+            $mailerMessage->send();
+        }
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return PHPMailer
+     */
+    private function prepareMailMessage(string $email): PHPMailer
+    {
+        $mail = new PHPMailer(true);
+        $mail->addAddress($email);
+        $mail->isSMTP();
+        $mail->SMTPAuth = true;
+        $mail->Host = $this->mailerParameters['host'];
+        $mail->Username = $this->mailerParameters['username'];
+        $mail->Password = $this->mailerParameters['password'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = $this->mailerParameters['port'];
+        if (isset($this->mailerParameters['fromEmail'])) {
+            $mail->setFrom($this->mailerParameters['fromEmail'], $this->mailerParameters['fromName'] ?? '');
+        }
+        $mail->isHTML(true);
+
+        return $mail;
+    }
+}
