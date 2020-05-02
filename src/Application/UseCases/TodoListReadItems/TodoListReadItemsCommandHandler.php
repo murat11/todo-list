466d1af0bbb3ee7d7f94c6d0b6c391cc324c7d62
Murat Erkenov <murat@11bits.net>
2020-05-02 13:09:33 +0300

    TodoListReadItemsCommandHandler prepared to be Controller

--- src/Application/UseCases/TodoListReadItems/TodoListReadItemsCommandHandler.php
+++ src/Application/UseCases/TodoListReadItems/TodoListReadItemsCommandHandler.php
@@ -3,12 +3,26 @@
 namespace App\Application\UseCases\TodoListReadItems;
 
 use App\Domain\TodoList\TodoListItem;
+use App\Domain\TodoList\TodoListManager\TodoListManager;
 use App\Domain\TodoList\TodoListManager\TodoListManagerAwareInterface;
 use App\Domain\TodoList\TodoListManager\TodoListManagerAwareTrait;
 
 class TodoListReadItemsCommandHandler implements TodoListManagerAwareInterface
 {
     use TodoListManagerAwareTrait;
+
+    /**
+     * @var TodoListManager
+     */
+    private $todoListManager;
+
+    /**
+     * @param TodoListManager $todoListManager
+     */
+    public function __construct(TodoListManager $todoListManager)
+    {
+        $this->todoListManager = $todoListManager;
+    }
 
     /**
      * @param TodoListReadItemsCommand $command
