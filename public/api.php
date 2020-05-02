6a7aed638320da1d7faa6bd723a83dc332ef758d
Murat Erkenov <murat@11bits.net>
2020-05-02 10:51:35 +0300

    symfony app env created

--- public/api.php
+++ public/api.php
@@ -1,20 +1,27 @@
 <?php
 
-use App\Infrastructure\Framework\Api\ApiRequest;
-use App\Infrastructure\Framework\Api\ApiRequestMatcher;
-use App\Infrastructure\Framework\Api\FrontController;
+use App\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-require '../vendor/autoload.php';
-require '../src/assemble.php';
+require dirname(__DIR__).'/config/bootstrap.php';
 
-$routes = include '../src/routes.php';
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
 
-$apiController = FrontController::createInstance();
-foreach ($routes as list($method, $path, $handlerClassName)) {
-    $apiController->registerHandler(
-        $requestHandlerFactory->createRequestHandler($handlerClassName),
-        new ApiRequestMatcher($method, $path)
-    );
+    Debug::enable();
 }
 
-$apiController->handle(ApiRequest::fromGlobals())->render();
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
